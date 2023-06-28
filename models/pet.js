d9ff6cb5134fa34033dc33a4a38b449ff6773470
ndoelger <doelgern@gmail.com>
2023-06-28 08:26:10 -0400

    Final

--- models/pet.js
+++ models/pet.js
@@ -2,13 +2,15 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const petSchema = {
-  image: {type: String, default: ""},
-  name: {type: String, default: ""},
-  type: {type: String, default: ""},
-  breed: {type: String, default: ""},
-  age: {type: Number, default: ""},
-  birthday: {type: Date, default: ""},
+  image: { type: String, default: "" },
+  name: { type: String, default: "" },
+  type: { type: String, default: "" },
+  breed: { type: String, default: "" },
+  age: { type: Number, default: 1 },
+  birthday: {
+    type: Date,
+    default: new Date(),
+  },
 };
 
 module.exports = mongoose.model("Pet", petSchema);
-
